01a0871fa64f93f573367302ea74e9a7e99c5948
BillyChilly808 <147418914+BillyChilly808@users.noreply.github.com>
2025-06-11 17:51:34 -1000

    add name to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer__container">
-      <p className="footer__text">Developed by Name Surname</p>
+      <p className="footer__text">Developed by Nethanial Malvecino</p>
       <p className="footer__year">2025</p>
     </footer>
   );
